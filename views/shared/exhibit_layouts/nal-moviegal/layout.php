95e723025466008867020cdb3de28e9291c458a5
Rachel Donahue <donahrm@gmail.com>
2014-12-03 13:26:13 -0500

    removed options from movie layout

--- views/shared/exhibit_layouts/nal-moviegal/layout.php
+++ views/shared/exhibit_layouts/nal-moviegal/layout.php
@@ -1,21 +1,5 @@
-<?php
-$showcasePosition = isset($options['showcase-position'])
-    ? html_escape($options['showcase-position'])
-    : 'none';
-$showcaseFile = $showcasePosition !== 'none' && !empty($attachments);
-$galleryPosition = isset($options['gallery-position'])
-    ? html_escape($options['gallery-position'])
-    : 'left';
-?>
-<?php if ($showcaseFile): ?>
-<div class="gallery-showcase <?php echo $showcasePosition; ?> with-<?php echo $galleryPosition; ?>">
-<?php
-        $attachment = array_shift($attachments);
-        echo $this->exhibitAttachment($attachment, array('imageSize' => 'fullsize'));
-    ?>
-</div>
-<?php endif; ?>
-<div class="gallery <?php if ($showcaseFile) echo "with-showcase $galleryPosition"; ?>">
+
+<div class="movie gallery">
 <div style="text-align:left;"><?php echo $this->shortcodes($text); ?></div>
 <?php $counter = 0; ?>
   <?php foreach ($attachments as $attachment): ?>
