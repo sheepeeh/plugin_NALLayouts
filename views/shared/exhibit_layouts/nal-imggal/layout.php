e809544e255f0a0ab9d19a31d92cd4a068a288d2
Rachel Donahue <donahrm@gmail.com>
2015-07-10 14:17:49 -0400

    make images link to item page, not file

--- views/shared/exhibit_layouts/nal-imggal/layout.php
+++ views/shared/exhibit_layouts/nal-imggal/layout.php
@@ -1,89 +1,114 @@
 <?php
 $showcasePosition = isset($options['showcase-position'])
-    ? html_escape($options['showcase-position'])
-    : 'none';
+? html_escape($options['showcase-position'])
+: 'none';
 $showcaseFile = $showcasePosition !== 'none' && !empty($attachments);
 $galleryPosition = isset($options['gallery-position'])
-    ? html_escape($options['gallery-position'])
-    : 'left';
+? html_escape($options['gallery-position'])
+: 'left';
 
-    $size = isset($options['file-size'])
-    ? html_escape($options['file-size'])
-    : 'thumbnail';
+$size = isset($options['file-size'])
+? html_escape($options['file-size'])
+: 'thumbnail';
 
 $width = isset($options['img-width'])
-    ? html_escape($options['img-width'])
-    : '';
+? html_escape($options['img-width'])
+: '';
 $showMetadata = isset($options['metadata-display'])
-    ? ($options['metadata-display'])
-    : '';
-    
+? ($options['metadata-display'])
+: '';
+
 ?>
 <?php if ($showcaseFile): ?>
-<div class="gallery-showcase <?php echo $showcasePosition; ?> with-<?php echo $galleryPosition; ?>">
-<?php
+    <div class="gallery-showcase <?php echo $showcasePosition; ?> with-<?php echo $galleryPosition; ?>">
+        <?php
         $attachment = array_shift($attachments);
         echo $this->exhibitAttachment($attachment, array('imageSize' => 'fullsize'));
-    ?>
-</div>
+        ?>
+    </div>
 <?php endif; ?>
 <div class="gallery <?php if ($showcaseFile) echo "with-showcase $galleryPosition"; ?>">
-<div style="text-align:left;"><?php echo $this->shortcodes($text); ?></div>
-<?php $counter = 0; ?>
-  <?php foreach ($attachments as $attachment): ?>
-        <?php $item = $attachment->getItem(); ?>
-        <?php $file = $attachment->getFile(); ?>
-      <?php if ($counter == 0): ?>
-        <div id="imggal-row">
-    <?php endif; ?>
-            <?php $counter++; ?>
-             <div class="exhibit-item exhibit-gallery-item" style=<?php echo '"width:' . $width . '"' ;?>>
-             <?php $altText = "Thumbnail for item, linking to full sized image."; ?>
-            <?php if  ($description = (metadata($item, array("Dublin Core", "Description")))): ?>
-            <?php $altText =  $description; ?>
-            <?php endif; ?> 
-            <?php echo file_markup($file, array('imageSize'=>$size, 'imgAttributes'=>array('alt' =>  "$altText", 'title' => metadata($item, array("Dublin Core", "Title"))))); ?>
-
-           <?php if ($attachment['caption'] || !empty($showMetadata)): ?>
-            <div class="exhibit-item-caption">
-            <?php 
-                    if ($attachment['caption']) {echo $attachment['caption']; }
-
-                    if (!empty($showMetadata)) {
-
-                        if (in_array("show-title", $showMetadata)) { 
-                            echo "<div class='exhibit-item-title'><a href="
-                            .exhibit_builder_exhibit_item_uri($item).">".metadata($item, array("Dublin Core", "Title"), 
-                                array('snippet'=>100))."</a></div>"; 
-                        }                   
-                        if (in_array("show-date", $showMetadata) && metadata($item, array("Dublin Core","Date"))) { 
-                            echo "<div class='exhibit-item-date'>("
-                            .metadata($item, array("Dublin Core", "Date"), array('snippet'=>100)).")</div>";
-                        }
-                        if (in_array("show-desc", $showMetadata) && metadata($item, array("Dublin Core","Description"))) { 
-                            echo '<div class="exhibit-item-description">'
-                            .metadata($item, array("Dublin Core", "Description"), array('snippet'=>150))."</div>";
-                        }
-                        if (in_array("show-script", $showMetadata) && metadata($item, array("Item Type Metadata","Transcription"))) { 
-                            echo "<div class='exhibit-item-transcript'>"
-                            .metadata($item, array("Item Type Metadata","Transcription"),array('snippet'=>150))."</div>"; 
-                        }
-                    }
-
-                ; ?>
-            </div>
-
-            <?php endif; ?>
-            </div>
-         
-            <?php if ($counter % 4 == 0 && $attachment != end($attachments)): ?>
-                </div>
-                <span class="break-row"></span>
-                <div id="imggal-row">
-            <?php endif; ?>
-
-
-        <?php endforeach; ?>
+    <div style="text-align:left;"><?php echo $this->shortcodes($text); ?>
     </div>
+    <?php $counter = 0; ?>
+    <?php if (count($attachments) > 0): ?>
+        <?php foreach ($attachments as $attachment): ?>
+            <?php $item = $attachment->getItem(); ?>
+            <?php $file = $attachment->getFile(); ?>
+            <?php $title = metadata($item, array("Dublin Core", "Title")); ?>
+            <?php if ($counter == 0): ?>
+                <div id="imggal-row">
+                <?php endif; ?>
+                <?php $counter++; ?>
+
+                <div class="exhibit-item exhibit-gallery-item" style=<?php echo '"width:' . $width . '"' ;?>>
+                   <?php 
+                        // echo attachment image
+
+                        // if  ($description = (metadata($item, array("Dublin Core", "Description")))) {
+                        //     $altTitle = $description;
+                        // } elseif ($caption = $attachment['caption']) {
+                        //     $altTitle = $caption;
+                        // } else {
+                        //     $altTitle = $title;                            
+                        // }
+
+                        // $altText = "Thumbnail for '$altTitle'."; 
+                        $fileOptions = array();
+
+                        $fileOptions['imgAttributes']['alt'] = "Thumbnail for the first (or only) page of $title.";
+                        $fileOptions['imgAttributes']['title'] = $title;
+                        $fileOptions['imageSize'] = $size;
+                        $fileOptions['linkAttributes']['href'] = exhibit_builder_exhibit_item_uri($item);
+                        $fileOptions['linkAttributes']['alt'] = "View more information about $title.";
+                        $fileOptions['linkAttributes']['title'] = "View more information about this item.";
+
+                        echo file_markup($file, $fileOptions, null); ?>
+
+                <?php if ($attachment['caption'] || !empty($showMetadata)): ?>
+                    <div class="exhibit-item-caption">
+                        <?php //echo caption and selected metadata elements
+                        if ($attachment['caption']) { echo $attachment['caption']; }
+
+                        if (!empty($showMetadata)) {
+
+                            if (in_array("show-title", $showMetadata)) { 
+                                echo "<div class='exhibit-item-title'><a href="
+                                .exhibit_builder_exhibit_item_uri($item).">".metadata($item, array("Dublin Core", "Title"), 
+                                    array('snippet'=>100))."</a></div>"; 
+                            }                   
+                            if (in_array("show-date", $showMetadata)) { 
+                                echo "<div class='exhibit-item-date'>("
+                                    .metadata($item, array("Dublin Core", "Date"), 
+                                        array('snippet'=>100)) . ")</div>";
+                            }
+                            if (in_array("show-desc", $showMetadata) && is_null(metadata($item, array('Dublin Core', 'Description'))) == false) { 
+                                echo '<div class="exhibit-item-description">'
+                                .metadata($item, array("Dublin Core", "Description"), array('snippet'=>150))."</div>";
+                            }
+                            if (in_array("show-script", $showMetadata) && is_null(metadata($item, array("Item Type Metadata","Transcription"))) == false) { 
+                                echo "<div class='exhibit-item-transcript'>"
+                                .metadata($item, array("Item Type Metadata","Transcription"),array('snippet'=>150))."</div>"; 
+                            }
+                    } ?>
+
 </div>
+<?php endif; ?>
+
+</div>
+
+<?php 
+// break every 4 images and insert a dividing line
+if ($counter % 4 == 0 && $attachment != end($attachments)): ?>
+</div>
+<span class="break-row"></span>
+<div id="imggal-row">
+<?php endif; ?>
+<?php if ($attachment == end($attachments)) { echo "</div>";} ?>
+
+<?php endforeach; ?>
+<?php endif; ?>
+</div>
+<!-- </div> -->
+
 
